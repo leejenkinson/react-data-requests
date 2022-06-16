a778757acc66fbfe3fc56985f07ab94953ceff07
leejenkinson <lee_jenkinson@msn.com>
2022-06-16 10:52:06 +0100

    created component that fetches data from an api using axios and useEffect function and then displays various elements of that data

---
+++ data-requests/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import EmployeeInfo from './EmployeeInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Employee info</h1>
+      <EmployeeInfo/>
+    </div>
+  );
+}
+
+export default App;
